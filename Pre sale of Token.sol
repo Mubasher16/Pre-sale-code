d96926f1c1b857742d90bd316f19565e172b6d66
Mubasher16 <102275712+Mubasher16@users.noreply.github.com>
2022-03-30 16:05:50 +0500

    Add files via upload

--- Pre sale of Token.sol
+++ Pre sale of Token.sol
@@ -1,22 +1,22 @@
-// Incomplete
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract preSale is ERC20{
-    uint8 private PriceofToken=100;
-    uint16 private availableTokens=500;
-    uint256 private ICOstartDate=1648618224;
-    uint256 private ICOendDate=1651296624;
+    uint256 private PriceofToken = 100;
+    uint256 private ICOstartDate = 1648618224;
+    uint256 private ICOendDate = 1651296624;
+    uint256 maxSupply = 10000;
+    uint256 private funds;
     constructor() ERC20 ("pakToken", "PKT"){}
-   // function validateUser(address account, uint256 amount) public{
-     //       address account = _msgSender(); }
-      modifier check(uint16 _availableToken , uint8 _priceofToken)
-    {
-       _;
+
+    function buyTokens(address account, uint256 amount) public payable {
+        require(account != address(0), "Invalid address");
+        require(block.timestamp<= ICOendDate);
+       uint256 tokensTranferred = (amount/ PriceofToken);       
+       require(totalSupply()+ tokensTranferred <= maxSupply, "maxsupply reached");
+       _mint(account, tokensTranferred);
+       funds+=amount;
+        
     }
-     function transfer(address to, uint256 amount) public virtual override returns (bool) {
-        address owner = _msgSender();
-        _transfer(owner, to, amount);
-        return true;
-    }
+
 }
